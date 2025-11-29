d93ff0decf32fd610abb99c2d3fe0dc1fbd8a507
Hamid Khanbeyki <Khanbeykihamidkhanbeyki@gmail.com>
2025-11-29 11:14:09 +0330

    add Js code for slider

--- js/test.js
+++ js/test.js
@@ -24,3 +24,32 @@ window.addEventListener("scroll", () => {
 
   lastScroll = currentScroll;
 });
+
+/* code for slider */
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+let slideInterval = setInterval(nextSlide, 5000); // 5 seconds
+
+function showSlide(index) {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  dots.forEach((dot) => dot.classList.remove("active"));
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function goToSlide(index) {
+  currentSlide = index;
+  showSlide(index);
+
+  // Reset timer when clicking dots
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, 5000);
+}
